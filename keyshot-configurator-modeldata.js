55dcf7d8f09f2045324ddb7aeaa99ef759da25b5
johalloran88 <jeremy.ohalloran@gmail.com>
2026-06-17 12:54:10 -0400

    Add files via upload

---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,295 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "light%20mocha.png",
+                    "multimaterials": [
+                        "241+227"
+                    ],
+                    "name": "Light Mocha"
+                },
+                {
+                    "checked": true,
+                    "icon": "graphite%20.png",
+                    "multimaterials": [
+                        "241+240"
+                    ],
+                    "name": "Graphite "
+                }
+            ],
+            "name": "Sanctuary Metallic Gloss  #1"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "light%20mocha-2.png",
+                    "multimaterials": [
+                        "526+512"
+                    ],
+                    "name": "Light Mocha"
+                },
+                {
+                    "checked": true,
+                    "icon": "seltzer%20gray.png",
+                    "multimaterials": [
+                        "526+513"
+                    ],
+                    "name": "Seltzer Gray"
+                }
+            ],
+            "name": "Sanctuary Metallic Gloss    #4"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "chrome%20polished%20.png",
+                    "multimaterials": [
+                        "248+242"
+                    ],
+                    "name": "Chrome Polished "
+                },
+                {
+                    "checked": true,
+                    "icon": "black%20electrophoresis.png",
+                    "multimaterials": [
+                        "248+243"
+                    ],
+                    "name": "Black Electrophoresis"
+                }
+            ],
+            "name": "Hardware Finishes  #1"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "chrome%20polished%20-2.png",
+                    "multimaterials": [
+                        "256+250"
+                    ],
+                    "name": "Chrome Polished "
+                },
+                {
+                    "icon": "black%20electrophoresis-2.png",
+                    "multimaterials": [
+                        "256+251"
+                    ],
+                    "name": "Black Electrophoresis"
+                },
+                {
+                    "icon": "chrome%20rough%20.png",
+                    "multimaterials": [
+                        "256+252"
+                    ],
+                    "name": "Chrome Rough "
+                },
+                {
+                    "icon": "light%20gold%20polished.png",
+                    "multimaterials": [
+                        "256+253"
+                    ],
+                    "name": "Light Gold Polished"
+                },
+                {
+                    "icon": "matte%20black%20powdercoat.png",
+                    "multimaterials": [
+                        "256+254"
+                    ],
+                    "name": "Matte Black Powdercoat"
+                },
+                {
+                    "checked": true,
+                    "icon": "smoked%20chrome.png",
+                    "multimaterials": [
+                        "256+255"
+                    ],
+                    "name": "Smoked Chrome"
+                }
+            ],
+            "name": "Hardware Finishes  #3"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "chrome%20polished%20-3.png",
+                    "multimaterials": [
+                        "572+566"
+                    ],
+                    "name": "Chrome Polished "
+                },
+                {
+                    "icon": "black%20electrophoresis-3.png",
+                    "multimaterials": [
+                        "572+567"
+                    ],
+                    "name": "Black Electrophoresis"
+                },
+                {
+                    "icon": "chrome%20rough%20-2.png",
+                    "multimaterials": [
+                        "572+568"
+                    ],
+                    "name": "Chrome Rough "
+                },
+                {
+                    "icon": "light%20gold%20polished-2.png",
+                    "multimaterials": [
+                        "572+569"
+                    ],
+                    "name": "Light Gold Polished"
+                },
+                {
+                    "icon": "matte%20black%20powdercoat-2.png",
+                    "multimaterials": [
+                        "572+570"
+                    ],
+                    "name": "Matte Black Powdercoat"
+                },
+                {
+                    "checked": true,
+                    "icon": "smoked%20chrome-2.png",
+                    "multimaterials": [
+                        "572+571"
+                    ],
+                    "name": "Smoked Chrome"
+                }
+            ],
+            "name": "Hardware Finishes   #9"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "chrome%20polished%20-4.png",
+                    "multimaterials": [
+                        "563+557"
+                    ],
+                    "name": "Chrome Polished "
+                },
+                {
+                    "icon": "black%20electrophoresis-4.png",
+                    "multimaterials": [
+                        "563+558"
+                    ],
+                    "name": "Black Electrophoresis"
+                },
+                {
+                    "icon": "chrome%20rough%20-3.png",
+                    "multimaterials": [
+                        "563+559"
+                    ],
+                    "name": "Chrome Rough "
+                },
+                {
+                    "icon": "light%20gold%20polished-3.png",
+                    "multimaterials": [
+                        "563+560"
+                    ],
+                    "name": "Light Gold Polished"
+                },
+                {
+                    "icon": "matte%20black%20powdercoat-3.png",
+                    "multimaterials": [
+                        "563+561"
+                    ],
+                    "name": "Matte Black Powdercoat"
+                },
+                {
+                    "checked": true,
+                    "icon": "smoked%20chrome-3.png",
+                    "multimaterials": [
+                        "563+562"
+                    ],
+                    "name": "Smoked Chrome"
+                }
+            ],
+            "name": "Hardware Finishes   #8"
+        },
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "chrome%20polished%20-5.png",
+                    "multimaterials": [
+                        "272+266"
+                    ],
+                    "name": "Chrome Polished "
+                }
+            ],
+            "name": "Hardware Finishes   #6"
+        },
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "chrome%20polished%20-6.png",
+                    "multimaterials": [
+                        "293+287"
+                    ],
+                    "name": "Chrome Polished "
+                }
+            ],
+            "name": "Hardware Finishes   #7"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Sanctuary Metallic Gloss  #1",
+                "Sanctuary Metallic Gloss    #4",
+                "Hardware Finishes  #1",
+                "Hardware Finishes   #6",
+                "Hardware Finishes   #7"
+            ],
+            "modelset": 1004,
+            "name": "Default",
+            "productoptiongroups": [
+                {
+                    "name": "Hardware",
+                    "productoptions": [
+                        {
+                            "icon": "handlekeypad_int_drop_illuminated.png",
+                            "modelset": 551041442,
+                            "name": "HANDLEKEYPAD_INT_DROP_ILLUMINATED"
+                        },
+                        {
+                            "checked": true,
+                            "icon": "handlekeypad_sep_drop_illuminated.png",
+                            "materialgroups": [
+                                "Hardware Finishes  #1"
+                            ],
+                            "modelset": 551040862,
+                            "name": "HANDLEKEYPAD_SEP_DROP_ILLUMINATED"
+                        },
+                        {
+                            "icon": "handlekeypad_sep_3spoke_illuminated.png",
+                            "materialgroups": [
+                                "Hardware Finishes  #3",
+                                "Hardware Finishes   #9"
+                            ],
+                            "modelset": 551040241,
+                            "name": "HANDLEKEYPAD_SEP_3SPOKE_ILLUMINATED"
+                        },
+                        {
+                            "icon": "handlekeypad_sep_1spoke_illuminated.png",
+                            "materialgroups": [
+                                "Hardware Finishes  #3",
+                                "Hardware Finishes   #8"
+                            ],
+                            "modelset": 551039205,
+                            "name": "HANDLEKEYPAD_SEP_1SPOKE_ILLUMINATED"
+                        },
+                        {
+                            "icon": "handlekeypad_int_3spoke_ulliminated.png",
+                            "modelset": 551022108,
+                            "name": "HANDLEKEYPAD_INT_3SPOKE_ULLIMINATED"
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "studios": []
+};
