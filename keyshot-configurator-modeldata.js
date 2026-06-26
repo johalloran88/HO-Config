e02412781330b1a4532be96455f8ea0be5c0fa25
johalloran88 <jeremy.ohalloran@gmail.com>
2026-06-26 10:50:50 -0400

    Add files via upload

---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,373 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "noir%20blue%20.png",
+                    "multimaterials": [
+                        "1142+1152"
+                    ],
+                    "name": "Noir Blue "
+                },
+                {
+                    "icon": "graphite%20.png",
+                    "multimaterials": [
+                        "1142+1154"
+                    ],
+                    "name": "Graphite "
+                },
+                {
+                    "icon": "soft%20taupe%20.png",
+                    "multimaterials": [
+                        "1142+1159"
+                    ],
+                    "name": "Soft Taupe "
+                },
+                {
+                    "checked": true,
+                    "icon": "frost%20white.png",
+                    "multimaterials": [
+                        "1142+1162"
+                    ],
+                    "name": "Frost White"
+                },
+                {
+                    "icon": "black%20cherry.png",
+                    "multimaterials": [
+                        "1142+1164"
+                    ],
+                    "name": "Black Cherry"
+                },
+                {
+                    "icon": "dawn%20gray%20.png",
+                    "multimaterials": [
+                        "1142+1166"
+                    ],
+                    "name": "Dawn Gray "
+                },
+                {
+                    "icon": "smoky%20quartz.png",
+                    "multimaterials": [
+                        "1142+1168"
+                    ],
+                    "name": "Smoky Quartz"
+                },
+                {
+                    "icon": "light%20mocha.png",
+                    "multimaterials": [
+                        "1142+1172"
+                    ],
+                    "name": "Light Mocha"
+                },
+                {
+                    "icon": "bone%20white%20.png",
+                    "multimaterials": [
+                        "1142+1176"
+                    ],
+                    "name": "Bone White "
+                },
+                {
+                    "icon": "seltzer%20gray.png",
+                    "multimaterials": [
+                        "1142+1179"
+                    ],
+                    "name": "Seltzer Gray"
+                },
+                {
+                    "icon": "linen%20gray%20.png",
+                    "multimaterials": [
+                        "1142+1180"
+                    ],
+                    "name": "Linen Gray "
+                },
+                {
+                    "icon": "driftwood%20.png",
+                    "multimaterials": [
+                        "1142+1185"
+                    ],
+                    "name": "Driftwood "
+                },
+                {
+                    "icon": "charcoal%20.png",
+                    "multimaterials": [
+                        "1142+1186"
+                    ],
+                    "name": "Charcoal "
+                }
+            ],
+            "name": "Sanctuary Metallic Gloss   #8"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "chrome%20polished%20.png",
+                    "multimaterials": [
+                        "1197+1198"
+                    ],
+                    "name": "Chrome Polished "
+                },
+                {
+                    "icon": "black%20electrophoresis.png",
+                    "multimaterials": [
+                        "1197+1200"
+                    ],
+                    "name": "Black Electrophoresis"
+                },
+                {
+                    "checked": true,
+                    "icon": "light%20gold%20polished.png",
+                    "multimaterials": [
+                        "1197+1201"
+                    ],
+                    "name": "Light Gold Polished"
+                },
+                {
+                    "icon": "matte%20black%20powdercoat.png",
+                    "multimaterials": [
+                        "1197+1202"
+                    ],
+                    "name": "Matte Black Powdercoat"
+                }
+            ],
+            "name": "Hardware Finishes  #14"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "swatch%206%20-%20light%20gray.png",
+                    "multimaterials": [
+                        "17+8"
+                    ],
+                    "name": "Swatch 6 - Light Gray"
+                },
+                {
+                    "icon": "swatch%2045%20-%20gray.png",
+                    "multimaterials": [
+                        "17+9"
+                    ],
+                    "name": "Swatch 45 - Gray"
+                },
+                {
+                    "icon": "swatch%2032%20-%20black.png",
+                    "multimaterials": [
+                        "17+10"
+                    ],
+                    "name": "Swatch 32 - Black"
+                },
+                {
+                    "icon": "swatch%2026%20-%20dark%20brown.png",
+                    "multimaterials": [
+                        "17+11"
+                    ],
+                    "name": "Swatch 26 - Dark Brown"
+                },
+                {
+                    "icon": "swatch%2033%20-%20tan.png",
+                    "multimaterials": [
+                        "17+12"
+                    ],
+                    "name": "Swatch 33 - Tan"
+                },
+                {
+                    "icon": "swatch%204%20-%20light%20tan.png",
+                    "multimaterials": [
+                        "17+13"
+                    ],
+                    "name": "Swatch 4 - Light Tan"
+                },
+                {
+                    "checked": true,
+                    "icon": "swatch%2011%20-%20dark%20beige.png",
+                    "multimaterials": [
+                        "17+14"
+                    ],
+                    "name": "Swatch 11 - Dark Beige"
+                },
+                {
+                    "icon": "swatch%205%20-%20beige.png",
+                    "multimaterials": [
+                        "17+15"
+                    ],
+                    "name": "Swatch 5 - Beige"
+                },
+                {
+                    "icon": "swatch%203%20-%20light%20beige.png",
+                    "multimaterials": [
+                        "17+16"
+                    ],
+                    "name": "Swatch 3 - Light Beige"
+                }
+            ],
+            "name": "Polycotton  #2"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "default.png",
+            "materialgroups": [
+                "Sanctuary Metallic Gloss   #8",
+                "Hardware Finishes  #14"
+            ],
+            "modelset": 1004,
+            "name": "Default",
+            "productoptiongroups": [
+                {
+                    "name": "Body Shape",
+                    "productoptions": [
+                        {
+                            "icon": "body_4cornerround.png",
+                            "materialgroups": [
+                                "Sanctuary Metallic Gloss   #8"
+                            ],
+                            "modelset": 1233539974,
+                            "name": "BODY_4CORNERROUND",
+                            "productoptiongroups": [
+                                {
+                                    "name": "Sub Component Group"
+                                }
+                            ]
+                        },
+                        {
+                            "icon": "body_2cornerround.png",
+                            "materialgroups": [
+                                "Sanctuary Metallic Gloss   #8"
+                            ],
+                            "modelset": 1233540094,
+                            "name": "BODY_2CORNERROUND"
+                        },
+                        {
+                            "checked": true,
+                            "icon": "body_2edges.png",
+                            "materialgroups": [
+                                "Sanctuary Metallic Gloss   #8"
+                            ],
+                            "modelset": 1233539850,
+                            "name": "BODY_2EDGES"
+                        }
+                    ]
+                },
+                {
+                    "name": "Door Orientation",
+                    "productoptions": [
+                        {
+                            "checked": true,
+                            "icon": "door%20closed.png",
+                            "materialgroups": [
+                                "Sanctuary Metallic Gloss   #8"
+                            ],
+                            "modelset": 1233570279,
+                            "name": "Door Closed",
+                            "productoptiongroups": [
+                                {
+                                    "name": "Hardware",
+                                    "productoptions": [
+                                        {
+                                            "checked": true,
+                                            "icon": "handlekeypad_sep_3spoke_illuminated.png",
+                                            "materialgroups": [
+                                                "Hardware Finishes  #14"
+                                            ],
+                                            "modelset": 1233600030,
+                                            "name": "HANDLEKEYPAD_SEP_3SPOKE_ILLUMINATED"
+                                        },
+                                        {
+                                            "icon": "handlekeypad_int_3spoke_ulliminated.png",
+                                            "materialgroups": [
+                                                "Sanctuary Metallic Gloss   #8"
+                                            ],
+                                            "modelset": 1233582576,
+                                            "name": "HANDLEKEYPAD_INT_3SPOKE_ULLIMINATED"
+                                        },
+                                        {
+                                            "icon": "handlekeypad_sep_drop_illuminated.png",
+                                            "materialgroups": [
+                                                "Hardware Finishes  #14"
+                                            ],
+                                            "modelset": 1233581996,
+                                            "name": "HANDLEKEYPAD_SEP_DROP_ILLUMINATED"
+                                        },
+                                        {
+                                            "icon": "handlekeypad_int_drop_illuminated.png",
+                                            "modelset": 1233570378,
+                                            "name": "HANDLEKEYPAD_INT_DROP_ILLUMINATED"
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            "icon": "door%20opened.png",
+                            "materialgroups": [
+                                "Hardware Finishes  #14",
+                                "Sanctuary Metallic Gloss   #8"
+                            ],
+                            "modelset": 1233540213,
+                            "name": "Door Opened",
+                            "productoptiongroups": [
+                                {
+                                    "name": "Door Trim",
+                                    "productoptions": [
+                                        {
+                                            "checked": true,
+                                            "icon": "interior%20trim%204.png",
+                                            "materialgroups": [
+                                                "Sanctuary Metallic Gloss   #8"
+                                            ],
+                                            "modelset": 1233612231,
+                                            "name": "Interior Trim 4"
+                                        },
+                                        {
+                                            "icon": "interior%20trim%203.png",
+                                            "materialgroups": [
+                                                "Polycotton  #2"
+                                            ],
+                                            "modelset": 1233611028,
+                                            "name": "Interior Trim 3"
+                                        },
+                                        {
+                                            "icon": "interior%20trim%202.png",
+                                            "modelset": 1233609825,
+                                            "name": "Interior Trim 2"
+                                        },
+                                        {
+                                            "icon": "interior%20trim%201.png",
+                                            "modelset": 1233609804,
+                                            "name": "Interior Trim 1"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "name": "Locking",
+                                    "productoptions": [
+                                        {
+                                            "icon": "2%20way%20locking%20bolts.png",
+                                            "materialgroups": [
+                                                "Sanctuary Metallic Gloss   #8",
+                                                "Hardware Finishes  #14"
+                                            ],
+                                            "modelset": 1602856146,
+                                            "name": "2 Way Locking Bolts"
+                                        },
+                                        {
+                                            "checked": true,
+                                            "icon": "2%20way%20locking%20plates.png",
+                                            "materialgroups": [
+                                                "Hardware Finishes  #14",
+                                                "Sanctuary Metallic Gloss   #8"
+                                            ],
+                                            "modelset": 1602856196,
+                                            "name": "2 way Locking Plates"
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "studios": []
+};
